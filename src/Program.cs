88ce4151746632c33173aae6831c44a83366b5c1
VictoriaKuzminova <vikakyzminova9@gmail.com>
2024-04-19 09:15:17 +0300

    Создание класса Student

--- src/Program.cs
+++ src/Program.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace src
 {
+    public class Student
+    {
+        public string Surname;
+        public string Name;
+        public int NumZachetka;
+
+        public Student(string surname, string name, int numZachetka)
+        {
+            Surname = surname;
+            Name = name;
+            NumZachetka = numZachetka;
+        }
+    }
+
 
     class Program
     {
